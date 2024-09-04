d0cd2e6ce72448a0ef16c3f8f7b1d2a7d76367d6
ranaemek <rana_emek@outlook.com>
2024-09-04 20:22:03 +0300

    Welcome with 3 messages

---
+++ chapter01/Exercise01_01.py
@@ -0,0 +1,11 @@
+# Author:Rana Emek
+# Email: rana_emek@outlook.com
+# Github: /ranaemek
+
+
+'''  (Display there different messages) Write a program that display welcome to python,Welcome to computer science ,and programming is fun'''
+
+#Display three messages
+print("Welcome to Python")
+print("Welcome to Computer Science")
+print("Programming is fun")
