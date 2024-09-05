4305309ce54e53901124f5490eb329c032fa7047
ranaemek <rana_emek@outlook.com>
2024-09-05 21:59:54 +0300

    Display Python pattern

---
+++ chapter01/Exercise01_03.py
@@ -0,0 +1,12 @@
+#Exercise01_03
+#(Display a pattern) Write a program that displays the following pattern:Write a program that displays the following pattern:
+
+
+#print Python pattern
+print("""
+####   #    #  ##### #   #   ###   #   #
+#    #  #  #     #   #   #  #    # ##  #
+####     #       #   #####  #    # # # #
+#        #       #   #   #  #    # #  ##
+#        #       #   #   #   ###   #   #
+""")
